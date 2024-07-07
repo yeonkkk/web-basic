eb2b52fe7f958ce3dca188e801b0302eaa6dee62
yeonkkk <tjddus0302@gmail.com>
2024-07-07 14:59:17 +0900

    입문 자바스크립트: 9. null, undefined

--- js-basic2/script.js
+++ js-basic2/script.js
@@ -1,9 +1,14 @@
-let number = 21;
-// let player = "joy";
-let player = window.prompt("좋아하는 선수 입력");
+let data;
+console.log(data); // undefined
 
-// 템플릿 리터럴 사용
-// 변수, 메소드 다 상관 X
-console.log(
-    `제가 좋아하는 선수는 ${player}, 그의 등번호는 ${number}`
-)
+data = "Hello";
+console.log(data); // Hello
+
+data = null;
+console.log(data); // null
+
+let data2 = true;
+console.log(typeof data2);
+
+data2 = false;
+console.log(data2);
