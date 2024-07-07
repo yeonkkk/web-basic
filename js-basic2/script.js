88887ba0ce6016c94dc37c0bd2ac41aaf42c4d09
yeonkkk <tjddus0302@gmail.com>
2024-07-07 14:41:52 +0900

    입문 자바스크립트: 6. 상수, prompt

--- js-basic2/script.js
+++ js-basic2/script.js
@@ -1,4 +1,13 @@
-let joy = 10;
-console.log(joy);
-joy = '조이';
-console.log(joy);
+let data;
+data = 5;
+console.log(data);
+data = 3;
+
+// 상수 값을 변경해서 에러
+// const data2 = 4;
+// console.log(data2);
+// data2 = 3;
+
+const promptMessage = "프롬프트 표시 문자";
+const result = window.prompt(promptMessage)
+console.log(result)
