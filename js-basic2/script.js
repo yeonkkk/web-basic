08d6149f589ced0730d42c38d5047e65f736df91
yeonkkk <tjddus0302@gmail.com>
2024-07-07 15:36:12 +0900

    입문 자바스크립트: 16. 함수 1

--- js-basic2/script.js
+++ js-basic2/script.js
@@ -1,14 +1,19 @@
-// console.log(document.querySelector('h2'));
-// console.log(document.querySelector('p'));
-// console.log(document.querySelector('#text'));
-// console.log(document.querySelector('.paragraph'));
+// 가능 (호이스팅 O)
+sayHello() 
 
-// console.log(document.getElementById('text'));
-// console.log(document.getElementById('p')); // null
+// 불가 (호이스팅 X)
+// sayBye() 
 
-const h2 = document.querySelector('h2');
-const p = document.getElementById('text');
+function sayHello() {
+    let hello = "Hello"
+    console.log(hello)
+}
 
-console.log(h2.textContent);
-h2.textContent = "헬로우 여러분!";
-console.log(h2.textContent);
+sayHello()
+
+// 익명 함수
+const sayBye = function() {
+    console.log("Bye")
+}
+
+sayBye()
