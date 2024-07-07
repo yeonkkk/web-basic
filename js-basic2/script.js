daad21a4efdd0dde1ca7a883f0dff621d07f50b3
yeonkkk <tjddus0302@gmail.com>
2024-07-07 16:01:19 +0900

    입문 자바스크립트: 20. 이벤트 객체

--- js-basic2/script.js
+++ js-basic2/script.js
@@ -1,10 +1,29 @@
-const inputType = document.querySelector("#typing");
-const inputClick = document.querySelector("#push");
+const btn1 = document.getElementById("one");
+const btn2 = document.getElementById("two");
+const btn3 = document.getElementById("three");
 
-inputType.onkeydown = function() {
-    console.log("타이핑되고 있어요.");
+const hadleClick = function(){
+    console.log("저를 클릭하셨나요?")
 }
 
-inputClick.onclick = function() {
-    console.log("클릭되고 있어요.");
+// 인자로 전달되는 함수: 콜백함수
+btn2.addEventListener('click', hadleClick);
+
+// 다른 핸들러도 등록 가능
+btn2.addEventListener('click', function(){
+    console.log("다른 핸들러 등록!")
+});
+
+// 핸들러 제거 가능
+btn2.removeEventListener('click', hadleClick);
+
+
+const hadleClick2 = function(event){
+    console.log(event.target)
 }
+
+// 이벤트 객체
+btn1.addEventListener('click', hadleClick2);
+btn2.addEventListener('click', hadleClick2);
+btn3.addEventListener('click', hadleClick2);
+
